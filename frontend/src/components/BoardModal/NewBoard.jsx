5593f8b7adb1149d2801df0e43221450f99ab2ee
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 16:59:01 -0700

    Created a component of new boards

--- frontend/src/components/BoardModal/NewBoard.jsx
+++ frontend/src/components/BoardModal/NewBoard.jsx
@@ -1,21 +1,43 @@
 import "./NewBoard.css"
+import { createBoard } from "../../services/apiClient"
+import { useState } from "react";
 
-export default function NewBoard() {
+export default function NewBoard({onClose}) {
+    const [titleInput, setTitleInput] = useState('');
+    const [authorInput, setAuthorInput] = useState('');
+    const [catInput, setCatInput] = useState('');
+
+    const handleCreateBoard = async() => {
+        await createBoard(titleInput, catInput, authorInput);
+    }
+
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-container" onClick={(e) => e.stopPropagation()}>
                 <span className="close" onClick={onClose}>
                     &times;
                 </span>
-                <form>
-                    <h2 className="title">Create a New Card</h2>
-                    <input type="text" placeholder="Enter a card title" required/>
-                    <input type="text" placeholder="Enter a card description" required/>
-                    <input type="text" placeholder="Search GIFs" required/>
-                    <button>Search</button>
-                    <input type="text" placeholder="Enter GIF url" />
-                    <button>Copy GIF URL</button>
-                    <input type="text" placeholder="Enter Owner" />
+                <form id="baord-form" onSubmit={handleCreateBoard}>
+                    <h2 className="title">Create a New Board</h2>
+                    <label>Title:
+                        <input 
+                        type="text" 
+                        value={titleInput} onChange={(e)=> setTitleInput(e.target.value)}
+                        required/>
+                    </label>
+                    <label>Category:</label>
+                    <select id="cat-dropdown" value={catInput} onChange={(e)=>setCatInput(e.target.value)}>
+                        <option value="">Select a category</option>
+                        <option value="celebration">Celebration</option>
+                        <option value="thank">Thank you</option>
+                        <option value="inspiration">Inspiration</option>
+                    </select>
+                    <label>Author:
+                        <input type="text" 
+                        value={authorInput} onChange={(e)=> setAuthorInput(e.target.value)}
+                        />
+                    </label>
+                    <button type="submit">Create a New Board</button>
                 </form>
             </div>
         </div>
