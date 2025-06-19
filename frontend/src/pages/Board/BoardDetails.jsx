742da709aeced20bdf1fb211ce48821c5c6e8744
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 16:51:24 -0700

    Fetched cards from API and passed them to cardList

--- frontend/src/pages/Board/BoardDetails.jsx
+++ frontend/src/pages/Board/BoardDetails.jsx
@@ -1,13 +1,28 @@
 import "./BoardDetails.css"
 import CardList from "../../components/CardList/CardList"
-import { Link } from "react-router"
+import { Link, useParams } from "react-router"
+import { useEffect, useState } from "react"
+import { getCards } from "../../services/apiClient"
 
-export default function Board() {
+export default function BoardDetails() {
+    const [cards, setCards] = useState([])
+    const params = useParams();
+
+    const getCardsData = async () => {
+        const results = await getCards(params.id);
+        setCards(results);
+    }
+
+    useEffect(() => {
+        getCardsData();
+    }, [])
+
     return(
         <div className="">
             <Link to="/">Back to Home</Link>
-            <button>Create a Card</button>
-            <CardList />
+            <CardList 
+            cards={cards}
+            setCards={setCards}/>
         </div>
     )
     
