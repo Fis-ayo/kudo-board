f3c847dd5a22a7c9aad2900df4b960b00fc9307f
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 16:48:00 -0700

    Fixed file error in board component and created component to show all boards

--- frontend/src/components/BoardList/BoardList.jsx
+++ frontend/src/components/BoardList/BoardList.jsx
@@ -1,16 +1,41 @@
-import "./BoardList.css"
-import Board from "./Board"
+import "./BoardList.css";
+import Board from "./Board";
+import NewBoard from "../BoardModal/NewBoard";
+import {useState} from 'react';
+import { deleteBoard } from "../../services/apiClient";
 
 export default function BoardList({ boards, setBoards }) {
-    console.log(boards);
+    const [clickedBoard, setClickedBoard] = useState(false);
+
+    const handleBoardClick = () => {
+        setClickedBoard(true);
+    }
+
+    const handleBoardDelete = async(boardId) => {
+        await deleteBoard(boardId);
+        setBoards(prevBoards => prevBoards.filter(board => board.id !== boardId))
+    }
+
     return (
-        <section className="movieList-container">
-            {boards.map((board) => (
-                <Board
-                    key={board.id}
-                    item={board}
-                />
-            ))}
-        </section>
+        <div>
+            <button
+                onClick={handleBoardClick}
+                className="newBoard">
+                Create a New Board
+            </button>
+            <section className="boardList-container">
+                {boards.map((board) => (
+                    <Board
+                        key={board.id}
+                        item={board}
+                        onClick={handleBoardClick}
+                        onDelete={() => handleBoardDelete(board.id)}
+                    />
+                ))}
+            </section>
+            {clickedBoard &&
+                <NewBoard
+                    onClose={() => setClickedBoard(false)} />}
+        </div>
     )
 }
