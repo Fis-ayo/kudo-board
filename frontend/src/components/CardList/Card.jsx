d9247f91ba123f6c21b2a183d10fc3f3200876c7
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 16:52:16 -0700

    Created component for card details and displayed each cards in CardList

--- frontend/src/components/CardList/Card.jsx
+++ frontend/src/components/CardList/Card.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react"
 import "./Card.css"
 
-export default function Card() {
+export default function Card({ item, onVote, onDelete }) {
+    const [vote, setVote] = useState(item.vote_count);
+
+    const handleVote = () => {
+        onVote();
+        setVote(vote + 1);
+    }
+
     return (
         <div>
-            <h3></h3>
-            <p></p>
-            <img />
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <img src={item.GIF_URL} />
             <div className="card-actions">
-                <button></button>
-                <button className="delete-btn"></button>
+                <button onClick={handleVote}>👍🏾{vote}</button>
+                <button onClick={onDelete} className="delete-btn">Delete</button>
             </div>
         </div>
     )
--- frontend/src/components/CardList/CardList.jsx
+++ frontend/src/components/CardList/CardList.jsx
@@ -1,5 +1,51 @@
-import "./CardList.css"
+import "./CardList.css";
+import Card from "./Card";
+import NewCard from "../CardModal/NewCard";
+import { useState } from 'react';
+import { deleteCard, upVote } from "../../services/apiClient";
+import { useParams } from "react-router";
 
-export default function CardList() {
-    
+export default function CardList({ cards, setCards }) {
+    const [clickedCard, setClickedCard] = useState(false);
+    const params = useParams();
+
+    const handleCardClick = () => {
+        setClickedCard(true)
+    }
+
+    const handleCardDelete = async (cardId) => {
+        await deleteCard(params.id, cardId);
+        setCards(prevCards => prevCards.filter(card => card.id !== cardId));
+    }
+
+    const handleUpVote = async (cardId) => {
+        await upVote(params.id, cardId);
+    }
+
+    return (
+        <div>
+            <button
+                onClick={handleCardClick}
+                className="newCard">
+                Create a New Card
+            </button>
+            <section className="cardList-container">
+                {cards.map((card) => (
+                    <Card
+                        key={card.id}
+                        item={card}
+                        onDelete={() => handleCardDelete(card.id)}
+                        onVote={() => handleUpVote(card.id)}
+                    />
+                ))}
+            </section>
+            {clickedCard &&
+                <NewCard
+                    onClose={() => setClickedCard(false)}
+                />
+            }
+        </div>
+    )
 }
+
+
