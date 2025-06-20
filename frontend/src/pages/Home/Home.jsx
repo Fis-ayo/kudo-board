fa6405775f570ea304fdb29bf7a567f483d55c46
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 23:54:05 -0700

    Corrected category naming

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
 
         const categoryMap = {
             "Celebration":"celebration",
-            "Thank You":"thank you",
+            "Thank You":"thank",
             "Inspiration":"inspiration"
         };
 
