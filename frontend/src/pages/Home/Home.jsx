4e32da7453b9dba3f0e3865127ee796fb8b228bc
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-19 17:05:57 -0700

    Created filters in a component and rendered it in the Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -7,11 +7,13 @@ import "./Home.css";
 
 export default function Home() {
     const [boards, setBoards] = useState([]);
+    const [allBoards, setAllBoards] = useState([])
     const [emptyMessage, setEmptyMessage] = useState("");
+    const [filterOption, setFilterOption] = useState("");
 
     const getData = async () => {
         const results = await getBoards();
-        console.log(results);
+        setAllBoards(results)
         setBoards(results);
     };
 
@@ -25,13 +27,44 @@ export default function Home() {
         else setBoards(results);
     };
 
+    const handleFilter = () => {
+        const results  = allBoards.slice();
+
+        if(filterOption === "All"){
+            getData();
+            return
+        };
+
+        if(filterOption === "Recent") {
+            const sorted = results.sort((a,b) => b.id-a.id);
+            setBoards(sorted);
+            return;
+        };
+
+        const categoryMap = {
+            "Celebration":"celebration",
+            "Thank You":"thank you",
+            "Inspiration":"inspiration"
+        };
+
+        const categoryValue = categoryMap[filterOption];
+        if(categoryValue) {
+            const filtered = results.filter(board => board.category === categoryValue);
+            setBoards(filtered);
+        }
+    }
+
+    useEffect(() => {
+        handleFilter();
+    }, [filterOption]);
+
     return (
         <main>
             <section className="toolbar-container">
                 <SearchBar
                     onSearch={handleSearch}
                 />
-                <FilterBoards />
+                <FilterBoards setFilterOption={setFilterOption} />
             </section>
             {emptyMessage ?
                 (<p>{emptyMessage}</p>) :
